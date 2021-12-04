aae9d745409c14fd230be11bd263c9c7d93def1c
MILTONSALAZAR2001 <miltsfour67448812@gmail.com>
2021-12-03 23:06:41 -0400

    ejercicio 7   sumando vectores

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,10 @@ public class Main {
         //Ejercicio2 ejercicio2 = new Ejercicio2();
         //ejercicio2.ejecutar();
 
-        Ejercicio3 ejercicio3 = new Ejercicio3();
-        ejercicio3.ejecutar();
+        //Ejercicio3 ejercicio3 = new Ejercicio3();
+        //ejercicio3.ejecutar();
+
+        Ejercicio7 ejercicio7 = new Ejercicio7();
+        ejercicio7.ejecutar();
     }
 }
